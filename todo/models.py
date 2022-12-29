ecc11d242047e07e4a4827cfff19d5ada61ffbfc
codesari <ahmtsr81@gmail.com>
2022-12-29 23:09:59 +0300

    model created

---
+++ todo/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Todo(models.Model):
+    PRIORİTY=(
+        (1,'High')
+        (2,'Medium')
+        (3,'Low')
+    )
+    task=models.CharField(max_length=50)
+    description=models.TextField(blank=True)
+    priority=models.SmallIntegerField(choices=PRIORİTY,default=3)
+    # kullanıcı front-end'te hicbirşey seçmezse default olarak az öneme sahip bir task olarak 3 default olarak seçilir
+    is_done=models.BooleanField(default=False)
+    updated_date=models.DateTimeField(auto_now=True)
+    created_date=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.task
